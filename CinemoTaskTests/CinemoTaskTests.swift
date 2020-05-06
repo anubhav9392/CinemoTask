2153d245336be7171020322f57de989c96384229
Anubhav Chandra <chandraseelan.g@vinculumgroup.com>
2020-05-06 20:19:14 +0530

    Initial Commit

---
+++ CinemoTaskTests/CinemoTaskTests.swift
@@ -0,0 +1,34 @@
+//
+//  CinemoTaskTests.swift
+//  CinemoTaskTests
+//
+//  Created by Vinculum on 6/5/2020.
+//  Copyright © 2020 Anubhav. All rights reserved.
+//
+
+import XCTest
+@testable import CinemoTask
+
+class CinemoTaskTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
